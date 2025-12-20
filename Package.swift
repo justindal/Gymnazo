f4f4795da7130db18c3966acfd744a33e531f6d5
Justin Daludado <25357531+justindal@users.noreply.github.com>
2025-12-20 14:45:22 -0500

    add support for swift tools 5.10+ and Swift 5, add strict concurrency checks, and format file

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.10
 
 import PackageDescription
 
@@ -6,25 +6,32 @@ let package = Package(
     name: "Gymnazo",
     platforms: [
         .macOS(.v14),
-        .iOS(.v17)
+        .iOS(.v17),
     ],
     products: [
         .library(
             name: "Gymnazo",
             targets: ["Gymnazo"]
         ),
-        .library(name: "Box2D", targets: ["Box2D"])
+        .library(name: "Box2D", targets: ["Box2D"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.29.1"),
+        .package(
+            url: "https://github.com/ml-explore/mlx-swift",
+            from: "0.29.1"
+        ),
         .package(
             url: "https://github.com/apple/swift-collections.git",
             .upToNextMinor(from: "1.3.0")
         ),
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+        .package(
+            url: "https://github.com/apple/swift-docc-plugin",
+            from: "1.0.0"
+        ),
     ],
     targets: [
-        .target(name: "Box2D",
+        .target(
+            name: "Box2D",
             path: "Vendor/Box2D",
             exclude: ["src/box2d.natvis", "src/CMakeLists.txt"],
             sources: ["src"],
@@ -35,20 +42,27 @@ let package = Package(
         ),
         .target(
             name: "Gymnazo",
-            dependencies: [.product(name: "MLX", package: "mlx-swift"),
-                           .product(name: "MLXNN", package: "mlx-swift"),
-                           .product(name: "MLXOptimizers", package: "mlx-swift"),
-                           .product(name: "MLXRandom", package: "mlx-swift"),
-                           .product(name: "Collections", package: "swift-collections"),
-                            "Box2D"
+
+            dependencies: [
+                .product(name: "MLX", package: "mlx-swift"),
+                .product(name: "MLXNN", package: "mlx-swift"),
+                .product(name: "MLXOptimizers", package: "mlx-swift"),
+                .product(name: "MLXRandom", package: "mlx-swift"),
+                .product(name: "Collections", package: "swift-collections"),
+                "Box2D",
             ],
             resources: [
                 .process("Resources")
+            ],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency")
             ]
+
         ),
         .testTarget(
             name: "GymnazoTests",
             dependencies: ["Gymnazo"]
         ),
-    ]
+    ],
+    swiftLanguageVersions: [.version("6"), .v5]
 )
