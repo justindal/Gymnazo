a79c2b0404916b6bb3a1df97e341ed71d2b3cdfe
Justin Daludado <25357531+justindal@users.noreply.github.com>
2026-01-06 13:23:12 -0500

    implement base Algorithm protocol

--- Sources/Gymnazo/RL/Algorithm.swift
+++ Sources/Gymnazo/RL/Algorithm.swift
@@ -2,13 +2,52 @@
 //  Algorithm.swift
 //  Gymnazo
 //
-//  Created by Justin Daludado on 2026-01-06.
-//
 
-protocol Algorithm {
-    var policy: String { get }
-    var env: Env { get }
-    var learningRate: Double { get }
-    
-    
+/// Learning rate schedule evaluated with progress remaining in [0, 1].
+public protocol LearningRateSchedule {
+    func value(at progressRemaining: Double) -> Double
+    // https://www.hackingwithswift.com/swift/5.2/callasfunction im not sure if this is appropriate here but TODO
+}
+
+/// Constant learning rate schedule.
+public struct ConstantLearningRate: LearningRateSchedule {
+    public let value: Double
+
+    public init(_ value: Double) {
+        self.value = value
+    }
+
+    public func value(at progressRemaining: Double) -> Double {
+        value
+    }
+}
+
+/// Base algorithm protocol
+public protocol Algorithm {
+    associatedtype PolicyType: Policy
+    associatedtype EnvType: Env
+
+    var policy: PolicyType { get }
+    var env: EnvType? { get set }
+
+    var learningRate: any LearningRateSchedule { get }
+    var currentProgressRemaining: Double { get set }
+
+    var numTimesteps: Int { get set }
+    var totalTimesteps: Int { get set }
+
+    mutating func setupModel()
+
+    @discardableResult
+    mutating func learn(totalTimesteps: Int) -> Self
+}
+
+public extension Algorithm {
+    mutating func updateProgressRemaining(numTimesteps: Int, totalTimesteps: Int) {
+        currentProgressRemaining = 1.0 - Double(numTimesteps) / Double(totalTimesteps)
+    }
+
+    func currentLearningRate() -> Double {
+        learningRate.value(at: currentProgressRemaining)
+    }
 }
