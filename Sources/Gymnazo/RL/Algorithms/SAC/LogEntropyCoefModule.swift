21281f3c1ccc8e85470cb4263c221cbffcb230e8
Justin Daludado <25357531+justindal@users.noreply.github.com>
2026-01-10 04:52:26 -0500

    add SAC utils

---
+++ Sources/Gymnazo/RL/Algorithms/SAC/LogEntropyCoefModule.swift
@@ -0,0 +1,26 @@
+//
+//  LogEntropyCoefModule.swift
+//  Gymnazo
+//
+
+import MLX
+import MLXNN
+
+/// Module wrapper for trainable log entropy coefficient.
+///
+/// This lets us use an optimizer.
+final class LogEntropyCoefModule: Module {
+    @ModuleInfo var logEntCoef: MLXArray
+
+    init(initialValue: Float) {
+        self.logEntCoef = MLX.log(MLXArray([initialValue]))
+        super.init()
+    }
+
+    func callAsFunction() -> MLXArray {
+        logEntCoef
+    }
+
+    var value: MLXArray { logEntCoef }
+    var entCoef: MLXArray { MLX.exp(logEntCoef) }
+}
