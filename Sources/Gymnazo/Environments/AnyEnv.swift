cde0a9d9df99eb383a2ebe76e8f3487569e09491
justindal <25357531+justindal@users.noreply.github.com>
2026-01-19 16:50:40 -0500

    feat: add type-erased Env

--- Sources/Gymnazo/Environments/AnyEnv.swift
+++ Sources/Gymnazo/Environments/AnyEnv.swift
@@ -1,63 +1,130 @@
-import MLX
+public struct AnyEnv<Observation, Action>: Env {
+    private var box: AnyEnvBoxBase<Observation, Action>
 
-/// Converts an environment into an `AnyEnv`.
-@inlinable
-public func asAnyEnv(_ env: any Env) -> AnyEnv {
-    guard let typed = env as? any Env<MLXArray, MLXArray> else {
-        preconditionFailure(
-            "Expected Env<MLXArray, MLXArray> but got \(type(of: env))"
-        )
+    public init<E: Env>(_ env: E) where E.Observation == Observation, E.Action == Action {
+        self.box = AnyEnvBox(env)
     }
-    return AnyEnv(typed)
-}
 
-/// A type-erased `Env` wrapper.
-public struct AnyEnv: Env {
-    public typealias Observation = MLXArray
-    public typealias Action = MLXArray
-    public typealias ObservationSpace = AnySpace
-    public typealias ActionSpace = AnySpace
+    public var observationSpace: any Space<Observation> {
+        box.observationSpace
+    }
 
-    private var env: any Env<MLXArray, MLXArray>
-
-    public let observationSpace: AnySpace
-    public let actionSpace: AnySpace
+    public var actionSpace: any Space<Action> {
+        box.actionSpace
+    }
 
     public var spec: EnvSpec? {
+        get { box.spec }
+        set { box.spec = newValue }
+    }
+
+    public var renderMode: RenderMode? {
+        get { box.renderMode }
+        set { box.renderMode = newValue }
+    }
+
+    public var unwrapped: any Env {
+        box.unwrapped
+    }
+
+    public mutating func step(_ action: Action) throws -> Step<Observation> {
+        try box.step(action)
+    }
+
+    public mutating func reset(seed: UInt64?, options: EnvOptions?) throws -> Reset<Observation> {
+        try box.reset(seed: seed, options: options)
+    }
+
+    @discardableResult
+    public func render() throws -> RenderOutput? {
+        try box.render()
+    }
+
+    public mutating func close() {
+        box.close()
+    }
+}
+
+private class AnyEnvBoxBase<Observation, Action> {
+    var observationSpace: any Space<Observation> {
+        fatalError("Not implemented")
+    }
+
+    var actionSpace: any Space<Action> {
+        fatalError("Not implemented")
+    }
+
+    var spec: EnvSpec? {
+        get { nil }
+        set {}
+    }
+
+    var renderMode: RenderMode? {
+        get { nil }
+        set {}
+    }
+
+    var unwrapped: any Env {
+        fatalError("Not implemented")
+    }
+
+    func step(_ action: Action) throws -> Step<Observation> {
+        fatalError("Not implemented")
+    }
+
+    func reset(seed: UInt64?, options: EnvOptions?) throws -> Reset<Observation> {
+        fatalError("Not implemented")
+    }
+
+    func render() throws -> RenderOutput? {
+        nil
+    }
+
+    func close() {}
+}
+
+private final class AnyEnvBox<E: Env>: AnyEnvBoxBase<E.Observation, E.Action> {
+    var env: E
+
+    init(_ env: E) {
+        self.env = env
+    }
+
+    override var observationSpace: any Space<E.Observation> {
+        env.observationSpace
+    }
+
+    override var actionSpace: any Space<E.Action> {
+        env.actionSpace
+    }
+
+    override var spec: EnvSpec? {
         get { env.spec }
         set { env.spec = newValue }
     }
 
-    public var renderMode: String? {
+    override var renderMode: RenderMode? {
         get { env.renderMode }
         set { env.renderMode = newValue }
     }
 
-    public var unwrapped: any Env { env.unwrapped }
-
-    public init(_ env: any Env<MLXArray, MLXArray>) {
-        self.env = env
-
-        guard let obsSpace = env.observationSpace as? any MLXSpace else {
-            preconditionFailure(
-                "AnyEnv requires an MLX observation space; got \(type(of: env.observationSpace))"
-            )
-        }
-        guard let actSpace = env.actionSpace as? any MLXSpace else {
-            preconditionFailure(
-                "AnyEnv requires an MLX action space; got \(type(of: env.actionSpace))"
-            )
-        }
-
-        self.observationSpace = AnySpace(obsSpace)
-        self.actionSpace = AnySpace(actSpace)
+    override var unwrapped: any Env {
+        env.unwrapped
     }
 
-    public mutating func step(_ action: MLXArray) -> Step<MLXArray> {
-        env.step(action)
+    override func step(_ action: E.Action) throws -> Step<E.Observation> {
+        try env.step(action)
     }
 
-    public mutating func reset(seed: UInt64?, options: [String: Any]?) -> Reset<MLXArray> {
-        env.reset(seed: seed, options: options)
+    override func reset(seed: UInt64?, options: EnvOptions?) throws -> Reset<E.Observation> {
+        try env.reset(seed: seed, options: options)
+    }
+
+    override func render() throws -> RenderOutput? {
+        try env.render()
+    }
+
+    override func close() {
+        env.close()
     }
 }
